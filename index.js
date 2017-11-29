4ae0dcc1ca05aa440c09537d5a8a1674143caba9
Megan Collier <meganraec@live.com>
2017-11-29 01:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ function happyHolidays() {
 happyHolidays();
 
 function happyHolidaysTo(name) {
-  var greeting = ("Happy holidays, ${name}!");
+  var greeting = 'Happy holidays, '${name}!'';
   return greeting;
 }
 
 happyHolidaysTo(you);
 
 function happyHolidayTo(holiday, name){
-  var greeting = ("Happy ${holiday}, ${name}!")
+  var greeting = 'Happy ${holiday}, '${name}!''
   return greeting;
 }
 
 happyHolidayTo('Independence Day, you');
 
 function holidayCountdown(holiday, days){
-  var greeting = ("It\'s ${days} days until ${holiday}!")
+  var greeting = 'It\'s '${days}' days until '${holiday}!';
   return greeting;
 }
 
